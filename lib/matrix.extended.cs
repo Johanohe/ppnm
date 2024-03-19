7f9ece612bcec5d105add7c110635976e9ecc41f
Johanohe <johannesohe@gmail.com>
2024-03-19 15:15:08 +0100

    extension to the matrix class and a new lib Makefile

---
+++ lib/matrix.extended.cs
@@ -0,0 +1,40 @@
+
+using static System.Math;
+public partial class matrix
+{
+    public static matrix RandomMatrix(int n = 3, int m = 3)
+    {
+        var rnd = new System.Random();
+        matrix mat = new matrix(n, m);
+        for (int i = 0; i < m; i++)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                mat[i][x] = rnd.NextDouble();
+            }
+
+        }
+        return mat;
+    }
+    public static matrix RandomSymMatrix(int n = 3)
+    {
+        matrix mat = RandomMatrix(n, n);
+        return mat + mat.T;
+    }
+    public void PrettyPrint(string s = "", string format = "{0,10:g3} ", double acc = 1e-6)
+    {
+        System.Console.WriteLine();
+        System.Console.WriteLine(s);
+        System.Console.WriteLine($"(0 when Abs(element) < {acc})");
+        for (int ir = 0; ir < this.size1; ir++)
+        {
+            for (int ic = 0; ic < this.size2; ic++)
+            {
+                if (Abs(this[ir, ic]) < acc) { System.Console.Write(format, 0); }
+                else { System.Console.Write(format, this[ir, ic]); }
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+}
