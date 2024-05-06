c5d91b5aaa136b61356822196cf7a9e1d4bc47c9
Johanohe <johannesohe@gmail.com>
2024-05-06 15:13:05 +0200

    updated Minimization homework to include downhill simplex minimization method

--- homework/Minimization/src/Minimize.cs
+++ homework/Minimization/src/Minimize.cs
@@ -1,7 +1,11 @@
 using System;
 using static System.Math;
+using System.Collections.Generic;
 public static class Minimize
 {
+
+    // Methods used in Newton
+
     public static matrix Hessian(Func<vector, double> phi, vector x)
     {
         matrix H = new matrix(x.size);
@@ -60,5 +64,171 @@ public static class Minimize
 
         } while (steps < maxSteps);
         return (x, steps);
+    }
+
+    // Start of methods used in DownhillSimplex
+
+    public static double Size(vector[] vertexs)
+    {
+        double s = 0;
+        for (int i = 1; i < vertexs.Length; i++) { s = Math.Max(s, (vertexs[0] - vertexs[i]).norm()); }
+        return s;
+    }
+    public static vector Reflection(vector high, vector centroid)
+    {
+        return centroid + (centroid - high);
+    }
+    public static vector Expansion(vector high, vector centroid)
+    {
+        return centroid + 2 * (centroid - high);
+    }
+    public static vector Contraction(vector high, vector centroid)
+    {
+        return centroid + 0.5 * (high - centroid);
+    }
+    public static void Reduction(vector[] vertexs, vector low, int minPos, vector vertexValues, Func<vector, double> f)
+    {
+        for (int i = 0; i < vertexs.Length; i++)
+        {
+            if (!(i == minPos))
+            {
+                vertexs[i] = 0.5 * (vertexs[i] + low);
+                vertexValues[i] = f(vertexs[i]);
+            }
+
+        }
+    }
+
+    public static (vector, int) DownhillSimplex(
+        Func<vector, double> f,
+        vector x,
+        double acc = 1e-3,
+        double vertSize = 10,
+        bool doTwice = true,
+        int startingStepCount = 0
+    )
+    {
+        int nSteps = startingStepCount;
+
+        // Initializing simplex
+        int vertexCount = x.size + 1;
+        int vecSize = x.size;
+        vector vertexValues = new vector(vertexCount);
+        double initSize = vertSize;
+        vector[] vertexs = new vector[vertexCount];
+        for (int i = 0; i < vertexCount; i++)
+        {
+            vertexs[i] = x.copy();
+            if (!(i == vertexCount - 1)) vertexs[i][i] += initSize;
+            vertexValues[i] = f(vertexs[i]);
+        }
+
+        // Initializing variables
+        int maxPos = 0;
+        int minPos = 0;
+        vector centroid = new vector(vecSize);
+        vector reflected = new vector(vecSize);
+        vector expanded = new vector(vecSize);
+        vector contracted = new vector(vecSize);
+        double maxVal = 0;
+        double minVal = 0;
+        double reflectedVal = 0;
+        double expandedVal = 0;
+        double contractedVal = 0;
+
+        // Loop start
+        while (true)
+        {
+            maxPos = 0;
+            minPos = 0;
+            maxVal = vertexValues[0];
+            minVal = vertexValues[0];
+            // Finding min and max positions
+            for (int i = 1; i < vertexCount; i++)
+            {
+                if (vertexValues[i] > maxVal)
+                {
+                    maxPos = i;
+                    maxVal = vertexValues[i];
+                }
+                if (vertexValues[i] < minVal)
+                {
+                    minPos = i;
+                    minVal = vertexValues[i];
+                }
+
+            }
+
+            // Resetting and finding centroid
+            for (int i = 0; i < centroid.size; i++) centroid[i] = 0;
+            for (int i = 0; i < vertexCount; i++)
+            {
+                if (!(i == maxPos)) centroid += vertexs[i] / (vertexCount - 1);
+            }
+
+            // Main algorithm
+            reflected = Reflection(vertexs[maxPos], centroid);
+            reflectedVal = f(reflected);
+            // try reflection
+            if (reflectedVal < minVal)
+            {
+                expanded = Expansion(vertexs[maxPos], centroid);
+                expandedVal = f(expanded);
+                // try expansion
+                if (expandedVal < reflectedVal)
+                {
+                    vertexs[maxPos] = expanded;
+                    vertexValues[maxPos] = expandedVal;
+                } // accept expansion
+                else
+                {
+                    vertexs[maxPos] = reflected;
+                    vertexValues[maxPos] = reflectedVal;
+                }
+                // accept reflection
+            }
+            else
+            {
+                if (reflectedVal < maxVal)
+                {
+                    vertexs[maxPos] = reflected;
+                    vertexValues[maxPos] = reflectedVal;
+                } // accept reflection
+                else
+                {
+                    contracted = Contraction(vertexs[maxPos], centroid);
+                    contractedVal = f(contracted);
+                    // try contraction
+                    if (contractedVal < maxVal)
+                    {
+                        vertexs[maxPos] = contracted;
+                        vertexValues[maxPos] = contractedVal;
+                    } // accept contraction
+                    else
+                    {
+                        Reduction(vertexs, vertexs[minPos], minPos, vertexValues, f);
+                    }// do reduction
+                }
+            }
+            nSteps += 1;
+            // Check if converged
+            if (Size(vertexs) < acc || nSteps > 99999) break;
+        }
+
+        // Finding minimum vector to return
+        minPos = 0;
+        for (int i = 0; i < vertexCount; i++)
+        {
+            if (vertexValues[i] < minVal)
+            {
+                minPos = i;
+                minVal = vertexValues[i];
+            }
+
+        }
+
+        // Runs twice to make sure minimum is reached
+        if (doTwice) return DownhillSimplex(f, vertexs[minPos], acc, initSize, false, nSteps);
+        return (vertexs[minPos], nSteps);
     }
 }
--- homework/Minimization/src/TestData.cs
+++ homework/Minimization/src/TestData.cs
@@ -7,21 +7,55 @@ public static class TestData
         double y = v[1];
         return (1 - x) * (1 - x) + 100 * (y - x * x) * (y - x * x);
     }
+
     private static double Himmelblau(vector v)
     {
         double x = v[0];
         double y = v[1];
         return (x * x + y - 11) * (x * x + y - 11) + (x + y * y - 7) * (x + y * y - 7);
     }
+
+    public static vector RosenbrockGrad(vector v)
+    {
+        double x = v[0];
+        double y = v[1];
+        double dfx = -2 * (1 - x) - 400 * x * (y - x * x);
+        double dfy = 200 * (y - x * x);
+        vector dfxy = new vector(dfx, dfy);
+        return dfxy;
+    }
+    public static vector HimmelblauGrad(vector v)
+    {
+        double x = v[0];
+        double y = v[1];
+        double dfx = 4 * x * (x * x + y - 11) + 2 * (x + y * y - 7);
+        double dfy = 2 * (x * x + y - 11) + 4 * y * (x + y * y - 7);
+        vector dfxy = new vector(dfx, dfy);
+        return dfxy;
+    }
+
+    private static double test(vector v)
+    {
+        double x = v[0];
+        double y = v[1];
+        return (x - 3) * (x - 3) + 5 * (y + 4) * (y + 4);
+    }
+
     public static void Main()
     {
         Func<vector, double> rosenbrock = Rosenbrock;
         Func<vector, double> himmelblau = Himmelblau;
         vector rosenStart = new vector(1, 2);
         vector himmelStart = new vector(0.5, 0.5);
-        (vector rosenAns, int rosenSteps) = Minimize.Newton(rosenbrock, rosenStart);
-        (vector himmelAns, int himmelSteps) = Minimize.Newton(himmelblau, himmelStart);
-        Console.WriteLine($"Rosenbrock minima starting from ({rosenStart[0]},{rosenStart[1]}): x = {rosenAns[0]}, y = {rosenAns[1]} , using {rosenSteps} steps");
-        Console.WriteLine($"Himmelblau minima starting from ({himmelStart[0]},{himmelStart[1]}): x = {himmelAns[0]}, y = {himmelAns[1]}, using {himmelSteps} steps");
+
+        (vector rosenAns, int rosenSteps) = Minimize.DownhillSimplex(rosenbrock, rosenStart, 1e-5);
+        (vector himmelAns, int himmelSteps) = Minimize.DownhillSimplex(himmelblau, himmelStart, 1e-5);
+        Console.WriteLine($"Rosenbrock minima using downhill simplex starting from ({rosenStart[0]},{rosenStart[1]}): x = {rosenAns[0]}, y = {rosenAns[1]}.\nUsing {rosenSteps} steps, function value {Rosenbrock(rosenAns)}\n");
+        (rosenAns, rosenSteps) = Minimize.Newton(rosenbrock, rosenStart);
+        Console.WriteLine($"Rosenbrock minima using Newton starting from ({rosenStart[0]},{rosenStart[1]}): x = {rosenAns[0]}, y = {rosenAns[1]}.\nUsing {rosenSteps} steps,function value {Rosenbrock(rosenAns)}\n");
+        Console.WriteLine($"Himmmelblau minima using downhill simplex starting from ({himmelStart[0]},{himmelStart[1]}): x = {himmelAns[0]}, y = {himmelAns[1]}.\nUsing {himmelSteps} steps, function value {Himmelblau(himmelAns)}\n");
+        (himmelAns, himmelSteps) = Minimize.Newton(himmelblau, rosenStart);
+        Console.WriteLine($"Himmelblau minima using Newton starting from ({himmelStart[0]},{himmelStart[1]}): x = {himmelAns[0]}, y = {himmelAns[1]}.\nUsing {himmelSteps} steps, function value {Himmelblau(himmelAns)}\n");
+
     }
 }
